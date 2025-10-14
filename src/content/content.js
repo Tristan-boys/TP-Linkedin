3283c87e5427cd75c7d8a9d12ccf26e430817537
Felix-Ecole <felix.ecole@laposte.net>
2025-10-14 23:56:50 +0200

    [FEAT] Optimisation and completion

--- src/content/content.js
+++ src/content/content.js
@@ -1,57 +1,107 @@
-console.info("content initialisation")
+console.info("content initialisation");
 
+// Récupère le signale de l'extension pour effectuer une action.
 chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
-	if (msg?.type == "collect") sendResponse(new LinkedinScrapper())
+
+	// Extrait l'identifiant du profil depuis l'URL.
+	let ID = msg.url.split("/").slice(-2, -1)[0]
+
+	// Si le type d'action souhaité est d'effectuer la récupération,
+	// Alors, lance la collecte d'informations et retourne le résultat.
+	if (msg.type == "collect") sendResponse(new LinkedinScrapper(ID))
 })
 
-// Linkedin Scrapper Tool
-// ----------------------
-// Tested with this profiles:
-// 	- https://www.linkedin.com/in/jeremy-ganem-b3200a63
-// 	- https://www.linkedin.com/in/kevin-tran-59ab5b283
-// 	- https://www.linkedin.com/in/lerirejaune
-// 	- https://www.linkedin.com/in/jessica-alba
-// ----------------------
 class LinkedinScrapper {
-	constructor() {
-		// Fonction interne pour le formatage des données textuelles.
-		function nbFormatter(e) {
-			e = e.innerText
-			return e.match(/\d+/gm).join("") + (e.includes("Plus") ? "+" : "")
-		}
+	constructor(ID) {
 
-		// Récupère le nom et le prénom de la personne du profil.
-		[this.firstname, this.lastname] = qs("h1").innerText.split(" ")
+		// Liste les variables qui contiendra les données récoltable du profile.
+		this.id, this.firstname, this.lastname, this.relations, this.followers
+		this.jobs, this.job, this.langs, this.skills
+
+		// ------------------------------------------------------------
+
+		// Récupère l'identifiant unique du profil.
+		this.id = ID;
+
+		// Récupère le nom et le prénom du profil.
+		[this.firstname, this.lastname] = qs("h1")[0].innerText.split(" ")
+
+		// ------------------------------------------------------------
 
 		// Récupère le nombre d'abonnés et le nombre de relations.
 		const FRDT = qs(".ph5.pb5 ul")[1].qs("span.t-bold")
 
-		// Si le type de "FRDT" est une "NodeList",
-		if (TrueTypeOf(FRDT) === NodeList) {
+		// Si on possède ces 2 informations,
+		if (FRDT.length == 2) {
 
-			// alors, Formate toute les informations.
-			this.follower = nbFormatter(FRDT[0])
-			this.relations = nbFormatter(FRDT[1])
+			// Alors, Formate-les et enregistre-les.
+			this.relations = this.nbFormatter(FRDT[1])
+			this.followers = this.nbFormatter(FRDT[0])
 		} else {
 
-			// Sinon, défini une valeur générique.
-			this.follower = this.relations = "0"
+			// Sinon, enregistre une valeur générique.
+			this.relations = this.followers = "0"
 
-			// Récupère et formate la valeur en fonction du type de donnée que l'on a obtenu.
-			if (FRDT.parentElement.innerText.includes("relation")) {
-				this.relations = nbFormatter(FRDT)
+			// Formate et enregistre l'information récupérer en fonction de ça catégorie.
+			if (FRDT[0].parentElement.innerText.includes("relation")) {
+				this.relations = this.nbFormatter(FRDT[0])
 			} else {
-				this.follower = nbFormatter(FRDT)
+				this.followers = this.nbFormatter(FRDT[0])
 			}
 		}
+
+		// ------------------------------------------------------------
+
+		// Récupère les accès au différentes sections de la page.
+		const section = Object.fromEntries(
+			[...document.querySelectorAll(".artdeco-card")]
+				.filter(x => x.querySelector("h2"))
+				.map(x => [x.innerText.split("\n")[0].toLowerCase(), x])
+		)
+
+		// Récupère les sections qui nous seront utile.
+		const [jobs, skills, langs] = [
+			section["expérience"],
+			section["compétences"],
+			section["langues"]
+		]
+
+		// ------------------------------------------------------------
+
+		// Si la sections des expériences professionnel existe,
+		// Alors, parcours celle-ci et récupère leurs données.
+		this.jobs = jobs ? (function () {
+			let result = []
+
+			for (let job of jobs.qs("a.full-width")) {
+				let data = [...job.qs("span")].map(x => x.innerText)
+				result.push({ job: data[1], company: data[3], period: data[6], city: data[9] })
+			}
+
+			return result
+		})() : null
+
+		// Récupère le poste actuel du profil.
+		this.job = this.jobs ? this.jobs[0] : null
+
+		// ------------------------------------------------------------
+
+		// Si la sections des compétences professionnel existe,
+		// Alors, récupère le nombre de compétences qu'il possède.
+		this.skills = skills ? this.nbFormatter(
+			skills.qs(".pvs-navigation__text")[0]
+		) : null
+
+		// Si la sections des languges maîtrisé existe,
+		// Alors, récupère le nombre de languges qu'il parle.
+		this.langs = langs ? this.nbFormatter(
+			langs.qs(".pvs-navigation__text")[0]
+		) : null
+	}
+
+	// Fonction formattant textuellement les nombres.
+	nbFormatter(e) {
+		if (e?.innerText) e = e.innerText
+		return e.match(/\d+/gm).join("") + (e.includes("Plus") ? "+" : "")
 	}
 }
-
-// le nom et prénom du profil => OK
-// le poste actuel du profil
-// le nombre d’abonnés => OK
-// le nombre de relations => OK
-// la liste des expériences professionnelles
-// la liste des certifications et formations
-// le nombre de compétences
-// le nombre de langues
--- src/content/tools.js
+++ src/content/tools.js
@@ -1,11 +1,10 @@
 // Créer un raccouci pour les querySelector permettant de retourner soit une liste d'élément, soit l'élément lui-même.
 HTMLDocument.prototype.qs = HTMLElement.prototype.qs = NodeList.prototype.qs = function(q) {
-	r = this.querySelectorAll(q); return (r.length > 1) ? r : r[0]
+	return this.querySelectorAll(q)
 }
 
 // Raccouci DOM pour les commandes communes.
 qs = (r) => document.qs(r)
-log = console.log
 
 // Fonction permettant de connaitre le vrai type d'un object.
 function TrueTypeOf(obj) {
