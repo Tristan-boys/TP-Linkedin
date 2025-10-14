4c93a29803efc7f7917d3eba13bb3eb0cea306fb
Felix-Ecole <felix.ecole@laposte.net>
2025-10-14 16:29:23 +0200

    [PROGRESS]: Integrate tools and scrapper into the project

--- src/background/background.js
+++ src/background/background.js
@@ -5,5 +5,5 @@ const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhY
 const SUPABASE_TABLE = "Profiles";
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message);
+	console.log(message);
 })
